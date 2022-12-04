34a5cf6da396b118d99e214ce3f61691fefc7141
ledungsbacken <ledungsbacken@gmail.com>
2022-12-04 15:30:37 +0100

    Better

--- day4/part2.js
+++ day4/part2.js
@@ -4,21 +4,19 @@ const rl = readline.createInterface({
     // input: require('fs').createReadStream('exampleInput.txt'),
 });
 
-let pairs = [];
-let nonoverlappingPairs = [];
+let overlappingPairs = [];
 rl.on('line', function (line) {
-    pairs.push(line);
     const [pairOne, pairTwo] = line.split(',').map(e => {
         return e.split('-').map(e => parseInt(e));
     });
 
-    if (pairOne[0] < pairTwo[0] && pairOne[1] < pairTwo[0]) {
-        nonoverlappingPairs.push(line);
-    } else if (pairOne[0] > pairTwo[0] && pairOne[0] > pairTwo[1]) {
-        nonoverlappingPairs.push(line);
+    if (pairOne[0] <= pairTwo[0] && pairOne[1] > pairTwo[0]) {
+        overlappingPairs.push(line);
+    } else if (pairOne[0] < pairTwo[1] && pairOne[1] <= pairTwo[0]) {
+        overlappingPairs.push(line);
     }
 });
 
 rl.on('close', function () {
-    console.log(pairs.length - nonoverlappingPairs.length);
+    console.log(overlappingPairs.length);
 });
