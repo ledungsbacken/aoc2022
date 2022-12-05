49760f8cf00552f40753d40ba2cf1c62fc089fe4
ledungsbacken <ledungsbacken@gmail.com>
2022-12-05 20:50:02 +0100

    Wop wop ing

---
+++ day5/part2.js
@@ -0,0 +1,72 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: require('fs').createReadStream('input.txt'),
+    // input: require('fs').createReadStream('exampleInput.txt'),
+});
+
+let stacksInput = [];
+let movesInput = [];
+let foundEmptyLine = false;
+rl.on('line', function (line) {
+    if (line === '') {
+        foundEmptyLine = true;
+        return;
+    }
+    if (foundEmptyLine) {
+        movesInput.push(line);
+        return;
+    }
+    stacksInput.push(line);
+});
+
+rl.on('close', function () {
+    const stackKeys = stacksInput[stacksInput.length - 1].trim().replaceAll('   ', ' ').split(' ');
+
+    const stacks = {};
+    {
+        const tempStacks = stacksInput.map(e => {
+            return e.replaceAll('    ', '[]').replaceAll(' ', '').replaceAll('][', '],[').split(',').map(d => d.replaceAll('[', '').replaceAll(']', ''));
+        });
+        for (const key of stackKeys) {
+            const index = parseInt(key) - 1;
+            stacks[key] = [];
+            for (let i = tempStacks.length - 2; i >= 0; i--) {
+                const crate = tempStacks[i][index];
+                if (crate === '') {
+                    continue;
+                }
+                stacks[key].push(crate);
+            }
+        }
+    }
+
+    const moves = movesInput.map(e => {
+        const fragments = e.split(' ');
+        return {
+            numToMove: parseInt(fragments[1]),
+            from: fragments[3],
+            to: fragments[5],
+        };
+    });
+
+    for (const move of moves) {
+        const numToMove = move.numToMove;
+        const from = stacks[move.from];
+        const to = stacks[move.to];
+
+        let crates = [];
+        for (let i = 0; i < numToMove; i++) {
+            const crate = from[from.length - 1];
+            from.pop();
+            crates.push(crate);
+        }
+        crates.reverse();
+        to.push(...crates);
+    }
+
+    let result = '';
+    for (const key of stackKeys) {
+        result += stacks[key][stacks[key].length - 1];
+    }
+    console.log(result);
+});
